66d2397b978808dfa464dbcb3fccc47860d07c7b
Conner Rhodes <connerrhodes.25@gmail.com>
2021-11-18 19:09:23 -0500

    Rendered index and notes html

--- Develop/server.js
+++ Develop/server.js
@@ -5,15 +5,24 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'));
   });
   app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'notes.html'));
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
   });
+app.get('/', (req, res) => {
+
+});
+app.get('/api/notes', (req, res) => {
+  res.json('Hello!');
+});
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
