6de705f0177710d6d1210219e57cac93087570f3
Conner Rhodes <connerrhodes.25@gmail.com>
2021-12-27 13:52:10 -0500

    Deploying to heroku

--- Develop/server.js
+++ Develop/server.js
@@ -3,6 +3,9 @@
 const express = require('express');
 // Import the 'path' module
 const path = require('path');
+const fs = require('fs')
+var uniqid = require('uniqid'); 
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,6 +13,22 @@ const PORT = process.env.PORT || 3001;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'))
+
+function createNewNote(body) {
+  const note = body;
+  const savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
+  
+  savedNotes.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, './db/db.json'),
+    JSON.stringify(savedNotes)
+  );
+  return note;
+}
+
+
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
@@ -17,12 +36,43 @@ app.get('/', (req, res) => {
   app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
   });
-app.get('/', (req, res) => {
 
-});
+
+
 app.get('/api/notes', (req, res) => {
-  res.json('Hello!');
-});
+  let results = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
+    res.json(results)
+  })
+
+app.post('/api/notes', (req, res) => {
+  req.body.id = uniqid();
+  console.log(req.body)
+  const note = createNewNote(req.body)
+  res.json(note)
+})
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
+
+app.get('/api/notes/:id', (req,res)=>{
+  let notearray = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
+  for (i=0; i < notearray.length; i++){
+    console.log(notearray[i])
+    if(notearray[i].id === req.params.id){
+      let note = notearray[i]
+      res.json(note)
+    }
+  }
+
+})
+app.delete('/api/notes/:id', (req,res)=>{
+  let notearray = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
+  for (i=0; i < notearray.length; i++){
+    console.log(notearray[i])
+    if(notearray[i].id === req.params.id){
+     let removedNote = notearray[i]
+     notearray.splice(removedNote, 1)
+    }
+  }
+
+})
